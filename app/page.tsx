8be0417387f0ce32dc4d852f349f14b5320fdcad
Amedu Believe Abraham <amedubelieve@gmail.com>
2026-08-11 14:35:55 +0100

    Set login page as homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/auth/dashboard/login");
+  redirect("auth/login");
 }
 
 // export default function HomePage() {
